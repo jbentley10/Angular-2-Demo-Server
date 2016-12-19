a672289a637d8df3fc397ab13e6a912428865b7e
Travis <travis@travisrea.com>
2016-12-19 10:00:56 -0800

    commented out the global directive

--- src/app/global.directive.ts
+++ src/app/global.directive.ts
@@ -25,3 +25,59 @@ export class GlobalDirective {
   csBulletThree = 'May help achieve ideal physique';
 
 }
+// import { Directive, Injectable, OnInit } from '@angular/core';
+// import {Http, Response, Headers, RequestOptions} from "@angular/http";
+// import {Observable} from 'rxjs/Rx';
+// import { DataService } from './data.service';
+//
+// @Directive({
+//   selector: '[appGlobal]',
+//   providers: [DataService]
+// })
+//
+// @Injectable()
+// export class GlobalDirective implements OnInit {
+//   data: any;
+//   url: string;
+//   corpName: string;
+//   corpAddresss: string;
+//   returnAddress: string;
+//   phone: string;
+//   email: string;
+//   price: string;
+//   trialPrice: string;
+//   productName: string;
+//   csBulletOne: string;
+//   csBulletTwo: string;
+//   csBulletThree: string;
+//
+//   constructor (private dataService: DataService ) {}
+//
+//   getData(): void {
+//     this.dataService.getData()
+//     .subscribe(
+//       response => this.data = response[0],
+//       error => console.error(error),
+//       () => {
+//         this.url = this.data.url;
+//         this.productName = this.data.productName;
+//         this.corpName = this.data.corpName;
+//         this.corpAddresss = this.data.corpAddress;
+//         this.returnAddress = this.data.returnAddress;
+//         this.phone = this.data.phone;
+//         this.email = this.data.email;
+//         this.price = this.data.price;
+//         this.trialPrice = this.data.trialPrice;
+//         this.csBulletOne = this.data.csBulletOne;
+//         this.csBulletTwo = this.data.csBulletTwo;
+//         this.csBulletThree = this.data.csBulletThree;
+//       }
+//
+//     )
+//   }
+//
+//   ngOnInit(){
+//     this.getData();
+//   }
+//
+// }
